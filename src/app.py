621087e69146305c13a469a3f548209b2ec74386
Sidney <sbatchelder@whoi.edu>
2026-01-16 15:01:01 -0500

    navbar cleanup

--- src/app.py
+++ src/app.py
@@ -1,16 +1,10 @@
 # app.py
 
-import datetime as dt
-from collections import defaultdict
-
 import dash
-from dash import dcc, Input, Output, State, no_update, ctx
 import dash_bootstrap_components as dbc
 
 from layout import create_layout
 from names import *
-import data_loader
-from utils import range_slider_marks
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
@@ -24,18 +18,6 @@ app.title = "Glider Dashboard"
 
 # Use function-based layout (nice for larger apps)
 app.layout = create_layout
-
-
-
-# @app.callback(
-#     Output("left-drawer", "is_open"),
-#     Input("open-drawer", "n_clicks"),
-#     State("left-drawer", "is_open"),
-#     prevent_initial_call=True,
-# )
-# def toggle_drawer(n_clicks, is_open):
-#     return not is_open
-
 
 server = app.server
 
--- src/layout.py
+++ src/layout.py
@@ -10,15 +10,7 @@ def make_navbar() -> dbc.Navbar:
     Top navigation bar. It iterates over Dash's page registry
     and builds links for each registered page.
     """
-    # pullout_btn = html.Div(
-    #     dbc.Button(
-    #         "Sidebar >>",
-    #         id="open-drawer",
-    #         n_clicks=0,
-    #         color="secondary",
-    #         size="sm",
-    #     ),
-    # )
+    # TODO WHOI image
 
     nav_items = [dbc.NavbarBrand("GliderApp", href="/")]
     for page in dash.page_registry.values():
@@ -55,92 +47,6 @@ def make_navbar() -> dbc.Navbar:
     return navbar
 
 
-# def time_slider_card():
-#     card = dbc.Card(
-#         [
-#             dbc.CardHeader(
-#                 dbc.Row(
-#                     [
-#                         dbc.Col(html.Div("Time Range"), className="fw-semibold"),
-#                         #dbc.Col(timefilter_buttons(), width="auto"),
-#                     ],
-#                     align="center",
-#                     className="g-2",
-#                 )
-#             ),
-#             dbc.CardBody(
-#                 [
-#                     dcc.RangeSlider(
-#                         id=ControlIds.TIME_RANGE,
-#                         className="mb-3",
-#                         min=0, max=1, step=1,
-#                         value=[0, 1],
-#                         marks={0: "0", 1: "1"},
-#                         updatemode="drag",  # makes the readout update as you drag
-#                         tooltip={
-#                             "placement": "bottom",
-#                             "always_visible": False,
-#                             "transform": "epochToLocal",  # or "epochToUTC"
-#                             "template": "{value}",  # optional, but explicit
-#                             "style": {"fontSize": "0.9rem"},
-#                         },
-#                     ),
-#                     html.Div(
-#                         id=TextIds.TIMERANGE_READOUT,
-#                         #className="mt-3 text-muted",
-#                         style={"fontSize": "0.85rem"},
-#                     ),
-#                 ]
-#             ),
-#         ],
-#         class_name="mb-3",
-#     )
-#     return card
-#
-# def gliders_card():
-#     card = dbc.Card(
-#         [
-#             dbc.CardHeader("Gliders"),
-#             dbc.CardBody([
-#                 dcc.Checklist(
-#                     id=ControlIds.GLIDER_CHECKLIST,
-#                     options=[],
-#                     value=[],
-#                 ),
-#                 dbc.Button(
-#                     "Refresh file list",
-#                     id=ControlIds.REFRESH_BTN_ID,
-#                     n_clicks=0,
-#                     size="sm",
-#                     className="mb-2",
-#                 ),
-#                 dbc.Alert(
-#                     id=TextIds.STATUS,
-#                     children="Waiting for data files in ./data/ ...",
-#                     color="info",
-#                     class_name="mb-0",
-#                 ),
-#             ]),
-#         ],
-#         class_name="mb-3",
-#     )
-#     return card
-#
-#
-# def left_pullout(content):
-#     # The pullout panel itself
-#     pullout_panel = dbc.Offcanvas(
-#         children=content,
-#         id=DivIds.LEFT_DRAWER,
-#         title="Sidebar",
-#         is_open=False,
-#         placement="start",  # slide from the left
-#         backdrop=False,  # don't dim / block the rest of the screen
-#         scrollable=True,
-#     )
-#     return pullout_panel
-
-
 def create_layout():
     # instead of dash.page_container, we create our own container with flex-column and w-100
     pages_container = html.Div(
@@ -158,13 +64,7 @@ def create_layout():
     layout = dbc.Container(
         [
             make_navbar(),
-            # left_pullout([
-            #     html.H5("Controls"),
-            #     gliders_card(),
-            #     time_slider_card(),
-            # ]),
             pages_container,
-
         ],
         fluid=True,
         className="p-0 d-flex flex-column vh-100",
