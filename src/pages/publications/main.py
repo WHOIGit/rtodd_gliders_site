4e1dfc4f37baff5a478589d26b5bd832c7924ce7
Sidney <sbatchelder@whoi.edu>
2026-04-09 21:58:08 -0400

    publication page re-title

--- src/pages/publications/main.py
+++ src/pages/publications/main.py
@@ -10,7 +10,7 @@ PUBLICATIONS_HTML_PATH = os.environ.get("PUBLICATIONS_HTML_PATH", "config/public
 
 html_text = (Path.cwd() / PUBLICATIONS_HTML_PATH).read_text(encoding="utf-8")
 layout = html.Div([
-            html.H1('WHOI Spray Glider Operations Publications',
+            html.H1('Publications Related to WHOI Spray Glider Operations',
                     style={"textAlign": "center", "marginBottom": "40px"}),
             dcc.Markdown(html_text, dangerously_allow_html=True)],
             style={"maxWidth": "840px", "margin": "0 auto", "padding": "40px 20px"},
